46298b5bddc1fb233eb0fe30d007640098b0b3b2
LKA7 <lukasz.sobiech@hotmail.co.uk>
2022-02-15 13:49:24 +0000

    fix formattting

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,27 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
-import {
-  Pagination as Paginationx,
-  Icon,
-} from "react-materialize";
+import { Pagination as Paginationx, Icon } from "react-materialize";
 
 function Pagination({ currentPage, totalCountries, paginate }) {
   return (
     <Paginationx
       activePage={currentPage}
       items={totalCountries}
-      leftBtn={<Icon onClick={() => (e) => paginate(e-1)}>chevron_left</Icon>}
+      leftBtn={<Icon onClick={() => (e) => paginate(e - 1)}>chevron_left</Icon>}
       maxButtons={8}
-      rightBtn={<Icon onClick={() => (e) => paginate(e+1)}>chevron_right</Icon>}
+      rightBtn={
+        <Icon onClick={() => (e) => paginate(e + 1)}>chevron_right</Icon>
+      }
       onSelect={(e) => paginate(e)}
     />
   );
 }
 
 Pagination.propTypes = {
-    currentPage: PropTypes.number,
-    totalCountries: PropTypes.number,
-    paginate: PropTypes.func
+  currentPage: PropTypes.number,
+  totalCountries: PropTypes.number,
+  paginate: PropTypes.func,
 };
 
 export default Pagination;
